9b4f2eb09ac6acf3d5c1e495edc6a2234cc43b79
Alan Curiel <alannitro17@gmail.com>
2019-06-04 15:48:21 -0500

    Add Sieve Of Eratosthenes
    Added a working version of the sieve of eratosthenes algorithm.

---
+++ SieveOfEratosthenes/SieveOfEratosthenes.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SieveOfEratosthenes
+{
+    class SieveOfEratosthenes
+    {
+        static void Main(string[] args)
+        {
+            bool getInput = true;
+            int n = 0;
+            while(getInput)
+            {
+                Console.WriteLine("Type a positive integar n to get all numbers under n that are prime.");
+                string input = Console.ReadLine();
+                if (isNumber(input))
+                {
+                    n = int.Parse(input);
+                    getInput = false;
+                }
+                else
+                {
+                    Console.WriteLine("Not a valid integar try again!");
+                }
+            }
+
+            bool[] prime = new bool[n+1];
+
+        for(int i = 0; i < n; i++) 
+        {
+             prime[i] = true; 
+        }
+
+        for(int p = 2; p*p <= n; p++) 
+        { 
+             
+            if(prime[p]) 
+            { 
+                
+                for(int i = p*p; i <= n; i += p) 
+                {   
+                    prime[i] = false; 
+                } 
+            }
+        } 
+          
+        
+        for(int i = 2; i <= n; i++) 
+        { 
+            if(prime[i]) 
+            {
+                Console.Write(i + " "); 
+            }
+        } 
+
+
+            Console.ReadKey();
+        }
+
+        private static bool isNumber(string input)
+        {
+            return Regex.IsMatch(input, @"^\d+$");
+        }
+    }
+}
